dc5d9f7995744d132d4dd2dfef6e8893dfb85ceb
GalvasDev <146465630+VictorGalvaoMercante@users.noreply.github.com>
2023-10-10 11:23:16 -0300

    Javascript

--- validarSenhas.js
+++ validarSenhas.js
@@ -1,2 +1,14 @@
 function validarSenha(){
+    let senha = document.getElementById('senha').value;
+    let confirmacao = document.getElementById('confirmacao').value;
+    let msg = document.getElementById('msg')
+
+    if(senha !== confirmacao){
+        msg.innerHTML = "A senha tem que ser iguais"
+    }
+    if(senha.length < 6){
+        msg.innerHTML = "A senha tem que ter no mínimo 6 dígitos"
+    }
+
+   
 }
